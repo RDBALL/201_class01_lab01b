a180b72a29c50ca939f7d14a0eb11f8ede03c5be
Robert Ball <bobball89@gmail.com>
2022-07-11 11:49:00 -0700

    initial commit to add html, css, js content

---
+++ app.js
@@ -0,0 +1,14 @@
+let userName = prompt('What name do you like to be called?');
+      console.log('The user\'s name is ' + userName)
+
+let favMovie = prompt('What is your favorite movie?');
+      console.log('The user\'s favorite movie is ' + favMovie)
+
+let favArtist = prompt('Who is your favorite musical artist?');
+      console.log('The user\'s favorite musical artist is: ' + favArtist);
+
+let favHobby = prompt('What is your favorite hobby?');
+      console.log('The user\'s favorite hobby is: ' + favHobby);
+
+
+alert('Hey ' + userName + '!\nI think it\'s awesome that your favorite movie is ' + favMovie + '. \n' + favArtist + ' is great.\nAnd ' + favHobby + '? don\'t get me started on how fun that is!');
